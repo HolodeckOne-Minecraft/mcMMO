3bf0d1cf25e79e8549288968dff36998e860ec45
Alan Wood <alan_wood@yahoo.com>
2023-07-07 10:02:03 -0400

    Clean up after build

--- build.cmd
+++ build.cmd
@@ -1,1 +1,1 @@
-@docker run -it -v "%CD%:/work" -v "%CD%\.m2:/root/.m2" --workdir /work maven:3-openjdk-18 mvn clean package install
+@docker run --rm -it -v "%CD%:/work" -v "%CD%\.m2:/root/.m2" --workdir /work maven:3-openjdk-18 mvn clean package install
